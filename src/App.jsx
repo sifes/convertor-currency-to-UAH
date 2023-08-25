4566c3e3ec9763e6609202cc844d4e20ba20c987
sifes <lonfark@gmail.com>
2023-08-25 09:25:28 +0300

    color, refact

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './index.scss';
 const initialCurrencies = [{ "currencyCodeA": 840, "currencyCodeB": 980, "date": 1690405273, "rateBuy": 36.65, "rateCross": 0, "rateSell": 37.4406 },
 { "currencyCodeA": 978, "currencyCodeB": 980, "date": 1690433173, "rateBuy": 40.65, "rateCross": 0, "rateSell": 41.8498 },
 { "currencyCodeA": 985, "currencyCodeB": 980, "date": 1690443992, "rateBuy": 0, "rateCross": 9.4089, "rateSell": 0 },
-{ "currencyCodeA": 203, "currencyCodeB": 980, "date": 1690443931, "rateBuy": 0, "rateCross": 1.7282, "rateSell": 0 },{"currencyCodeA":826,"currencyCodeB":980,"date":1690443976,"rateBuy":0,"rateCross":48.448,"rateSell":0},]
+{ "currencyCodeA": 203, "currencyCodeB": 980, "date": 1690443931, "rateBuy": 0, "rateCross": 1.7282, "rateSell": 0 }, { "currencyCodeA": 826, "currencyCodeB": 980, "date": 1690443976, "rateBuy": 0, "rateCross": 48.448, "rateSell": 0 },]
 const currencies = [
-	{ id: 978, name: 'EUR' },
-	{ id: 840, name: 'USD' },
-	{ id: 826, name: 'GBR' },
-	{ id: 985, name: 'PLN' },
-	{ id: 203, name: 'CZK' },
+  { id: 978, name: 'EUR' },
+  { id: 840, name: 'USD' },
+  { id: 826, name: 'GBR' },
+  { id: 985, name: 'PLN' },
+  { id: 203, name: 'CZK' },
 ];
 
 function App() {
@@ -24,8 +24,8 @@ function App() {
       })
       .catch((err) => { console.log(err) })
   }, [])
-  const currencyFrom = allCurrencies.find(item => item.currencyCodeA === currencyIndexFrom && item.currencyCodeB===980)
-  const curs = currencyFrom.rateCross ? currencyFrom.rateCross : (currencyFrom.rateBuy+currencyFrom.rateSell)/2
+  const currencyFrom = allCurrencies.find(item => item.currencyCodeA === currencyIndexFrom && item.currencyCodeB === 980)
+  const curs = currencyFrom.rateCross ? currencyFrom.rateCross : (currencyFrom.rateBuy + currencyFrom.rateSell) / 2
 
   return (
     <div className="App">
@@ -40,14 +40,14 @@ function App() {
             ))}
           </ul>
           <div>
-            <input type='number' value={inputValue} onChange={e=> {
+            <input type='number' value={inputValue} onChange={e => {
               setInputValue(e.target.value)
-              }} placeholder={'enter the value'} />
+            }} placeholder={'enter the value'} />
           </div>
         </div>
         <div className='currencyTo'>
           <p>in UAH</p>
-          <span>{(curs*inputValue).toFixed(2)}</span>
+          <span>{(curs * inputValue).toFixed(2)}</span>
         </div>
       </div>
     </div>
